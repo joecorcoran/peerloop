4c78a108f75606c34c1aaf1df2e81b1da51dde08
Joe Corcoran <joe@corcoran.io>
2016-05-16 20:34:36 +0200

    Lots of hackz

--- client.js
+++ client.js
@@ -4,20 +4,33 @@ var ChatLog = function(container) {
   this.container = document.getElementById(container);
 };
 
-ChatLog.prototype.print = function(msg) {
-  var textNode = document.createTextNode(msg),
+ChatLog.prototype.printLine = function(text) {
+  var textNode = document.createTextNode(text),
       line = document.createElement('p');
   line.appendChild(textNode);
   this.container.appendChild(line);
 };
 
+ChatLog.prototype.printMessage = function(data) {
+  var uidNode = document.createTextNode(data.uid),
+      msgNode = document.createTextNode(' ' + data.message),
+      span = document.createElement('span'),
+      line = document.createElement('p');
+  span.style.color = '#' + data.uid;
+  span.appendChild(uidNode);
+  line.appendChild(span);
+  line.appendChild(msgNode);
+  this.container.appendChild(line);
+};
+
 var ChatClient = function(log) {
-  var config = { iceServers: [{ url: 'stun:23.21.150.121' }] },
+  var config = { iceServers: [{ url: 'stun:stun.stunprotocol.org' }] },
       connection = { optional: [{'DtlsSrtpKeyAgreement': true}] };
 
   this.log = log;
   this.conn = new RTCPeerConnection(config, connection);
   this.channel = null;
+  this.uid = Math.floor(Math.random()*16777215).toString(16);
 
   this.initConn();
 };
@@ -27,7 +40,8 @@ ChatClient.prototype.initConn = function() {
     if (e.candidate == null && this.conn.localDescription.type == 'offer') {
       var offer = JSON.stringify(this.conn.localDescription);
       console.log('Offer', offer);
-      this.log.print('Send this code to a friend: ' + btoa(offer));
+      this.log.printLine('Send this to someone you know:');
+      this.log.printLine(btoa(offer));
     }
   }.bind(this);
 
@@ -53,14 +67,14 @@ ChatClient.prototype.createChannel = function() {
 
 ChatClient.prototype.setupChannel = function() {
   this.channel.onopen = function (e) {
-    this.log.print('Chat ready!');
     console.log('Data channel open');
+    document.dispatchEvent(new Event('chatready'));
   }.bind(this);
   this.channel.onmessage = function (e) {
     if (e.data.charCodeAt(0) == 2) { return }
     var data = JSON.parse(e.data);
     console.log(data);
-    this.log.print(data.message);
+    this.log.printMessage(data);
   }.bind(this);
 };
 
@@ -86,7 +100,8 @@ ChatClient.prototype.answer = function(offer) {
 
       var answer = JSON.stringify(answer);
       console.log('Created local answer', answer);
-      this.log.print('Send this response to your friend: ' + btoa(answer));
+      this.log.printLine('Thanks! Send this confirmation code back to the person who invited you:');
+      this.log.printLine(btoa(answer));
     }.bind(this),
     function () {
       console.warn('Could not create answer');
@@ -101,32 +116,43 @@ ChatClient.prototype.connect = function(answer) {
 };
 
 ChatClient.prototype.sendMessage = function(msg) {
-  this.channel.send(JSON.stringify({ message: msg }));
+  var data = { uid: this.uid, message: msg };
+  this.channel.send(JSON.stringify(data));
+  this.log.printMessage(data);
 };
 
 // Move this into better structure
 var log = new ChatLog('chat');
 var client = new ChatClient(log);
 
-function offer() {
+document.getElementById('invite').addEventListener('click', function() {
+  log.printLine('Creating channel and generating invitation code...');
   client.createChannel();
   client.offer();
-};
+});
 
-function answer() {
-  var offer = prompt('Paste code');
+document.getElementById('accept').addEventListener('click', function() {
+  var offer = prompt('Paste invitation code');
   client.answer(offer);
-};
+});
 
-function connect() {
-  var answer = prompt('Paste your friend\'s response');
+document.getElementById('confirm').addEventListener('click', function() {
+  var answer = prompt('Paste confirmation code');
+  log.printLine('Waiting for connection...');
   client.connect(answer);
-};
+});
 
-function sendMessage() {
-  var txt = document.getElementById('message'),
-      msg = txt.value;
-  client.sendMessage(msg);
-  log.print(msg);
+document.addEventListener('chatready', function() {
+  log.printLine('Connection ready. Time to chat!');
+  var elements = document.getElementsByClassName('chatready');
+  Array.prototype.forEach.call(elements, function(element) {
+    element.disabled = false;
+  });
+});
+
+document.getElementById('message').addEventListener('submit', function(e) {
+  e.preventDefault();
+  var txt = document.getElementById('message-text');
+  client.sendMessage(txt.value);
   txt.value = '';
-};
+}, false);
