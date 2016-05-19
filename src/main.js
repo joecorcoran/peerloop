2a5d18b0d86adcb987e71fd1293a6e83dac53e96
Joe Corcoran <joe@corcoran.io>
2016-05-19 08:41:19 +0200

    Template strings

--- src/main.js
+++ src/main.js
@@ -119,10 +119,10 @@
 
     printMessage(data) {
       var uidNode = document.createTextNode(data.uid),
-          msgNode = document.createTextNode(' ' + data.message),
+          msgNode = document.createTextNode(` ${data.message}`),
           span = document.createElement('span'),
           line = document.createElement('p');
-      span.style.color = '#' + data.uid;
+      span.style.color = `#${data.uid}`;
       span.appendChild(uidNode);
       line.appendChild(span);
       line.appendChild(msgNode);
